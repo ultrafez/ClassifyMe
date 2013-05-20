d4e05caa7713c9b6c4cdcc25c1b0f4baacb6e997
Alex Silcock <alex@alexsilcock.net>
2013-05-20 18:22:16 +0100

    Added switching from single-row to multiple-row (where assessments can be entered manually). Module mark is automatically calculated and shown for multiple-row modules.

--- js/calculator.js
+++ js/calculator.js
@@ -1,14 +1,14 @@
 function CalculatorCtrl($scope) {
   $scope.years = [
     {"year": 2, "modules": [
-      // {"name": "Test Module", "credits": 10, "assessments": [
+      // {"name": "Test Module", "isSingleRow": false, "credits": 10, "assessments": [
       //   {"name": "Assignment", "weight": 50, "mark": 80},
       //   {"name": "Exam", "weight": 50, "mark": 74}
       // ]},
-      // {"name": "Test Module 2", "credits": 10, "assessments": []},
+      // {"name": "Test Module 2", "isSingleRow": true, "credits": 10, "assessments": []},
     ]},
     {"year": 3, "modules": [
-      // {"name": "Test Module 3", "credits": 120, "assessments": []},
+      // {"name": "Test Module 3", "isSingleRow": true, "credits": 120, "assessments": []},
     ]}
   ];
 
@@ -28,27 +28,7 @@ function CalculatorCtrl($scope) {
     }
   });
 
-  $scope.addModule = function(yearIndex) {
-    $scope.years[yearIndex].modules.push({"name": "", "credits": 10, "assessments": []});
-  }
-
-  $scope.deleteModule = function(year, moduleIndex) {
-    year.modules.splice(moduleIndex, 1);
-  }
-
-  $scope.addAssessment = function(module) {
-    module.assessments.push({"name": "", "weight": "", "mark": ""});
-  }
-
-  $scope.duplicateAssessment = function(module, assessmentIndex) {
-    console.log(module.assessments[assessmentIndex]);
-    module.assessments.push(angular.copy(module.assessments[assessmentIndex]));
-  }
-
-  $scope.deleteAssessment = function(module, assessmentIndex) {
-    module.assessments.splice(assessmentIndex, 1);
-  }
-
+  /* YEARS */
   $scope.totalCredits = function(year) {
     total = 0;
     angular.forEach(year.modules, function(value, key) {
@@ -63,4 +43,56 @@ function CalculatorCtrl($scope) {
   $scope.creditsNeeded = function(year) {
     return 120 - $scope.totalCredits(year);
   }
+
+
+  /* MODULES */
+  $scope.addModule = function(yearIndex) {
+    $scope.years[yearIndex].modules.push({"name": "", "credits": 10, "isSingleRow": true, "assessments": [{"name": "Whole Module", "weight": 100, "mark": 70}] });
+  }
+
+  $scope.deleteModule = function(year, moduleIndex) {
+    year.modules.splice(moduleIndex, 1);
+  }
+
+  // Calculate the mark for the module as a weighted sum of the marks of the assessments.
+  $scope.moduleMark = function(module) {
+    moduleMark = 0;
+    angular.forEach(module.assessments, function(assessment, key) {
+      if (assessment.mark !== undefined && assessment.weight !== undefined) {
+        moduleMark += assessment.mark * (assessment.weight / 100);
+      }
+    });
+    return moduleMark;
+  }
+
+  // Get the total weighting of the assessments in the module.
+  $scope.totalWeight = function(module) {
+    total = 0;
+    angular.forEach(module.assessments, function(assessment, key) {
+      if (assessment.weight !== undefined) {
+        total += assessment.weight;
+      }
+    });
+    return total;
+  }
+
+  // Convert a single-row module (where the user can enter their mark) into one where the assessments can be entered manually.
+  $scope.convertToMultiple = function(module) {
+    module.isSingleRow = false;
+  }
+
+  /* ASSESSMENTS */
+  $scope.addAssessment = function(module) {
+    module.assessments.push({"name": "", "weight": "", "mark": ""});
+  }
+
+  $scope.duplicateAssessment = function(module, assessmentIndex) {
+    console.log(module.assessments[assessmentIndex]);
+    module.assessments.push(angular.copy(module.assessments[assessmentIndex]));
+  }
+
+  $scope.deleteAssessment = function(module, assessmentIndex) {
+    module.assessments.splice(assessmentIndex, 1);
+  }
+
 }
